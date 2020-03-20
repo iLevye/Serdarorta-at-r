4323b74b28641c44da8b6bde3dfa5a2e84f7313b
Emre Can Yılmaz <e.canyilmaz@gmail.com>
2020-03-20 18:20:01 +0300

    typo

--- src/Service/SerdarOrtac.php
+++ src/Service/SerdarOrtac.php
@@ -1,7 +1,7 @@
 <?php
 
 
-namespace App\Services;
+namespace App\Service;
 
 
 class SerdarOrtac
--- src/Service/Telegram.php
+++ src/Service/Telegram.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Services;
+namespace App\Service;
 
 use Http\Adapter\Guzzle6\Client;
 use Http\Factory\Guzzle\RequestFactory;
